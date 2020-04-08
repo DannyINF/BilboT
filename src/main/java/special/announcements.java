73de80c85be1d226c07c221c196a0bdd06059ada
Moorhuhn <danny.schober@gmx.de>
2020-04-08 23:34:54 +0200

    Announcements wurden erneuert und angepasst

--- src/main/java/special/announcements.java
+++ src/main/java/special/announcements.java
@@ -21,35 +21,42 @@ public class announcements extends ListenerAdapter {
                 int random = ThreadLocalRandom.current().nextInt(0, 7);
                 switch (random) {
                     case 0:
-                        embed.setTitle("#TolkienTrotztCorona");
-                        embed.setDescription("Wir veranstalten zusammen mit der Deutschen Tolkien Gesellschaft die Tolkien Reading Days! Informationen dazu in " + event.getGuild().getTextChannelById("407960159227215872").getAsMention() + ".");
+                        embed.setTitle("\uD83D\uDCDA Der Lyrikabend:");
+                        embed.setDescription("Jede Woche findet auf dem Server der Lyrikabend statt. Dabei werden ein oder mehrere Kapitel vorgelesen und danach diskutiert." +
+                                "Um teilnehmen zu k\u00f6nnen, muss man seine Buchausgabe angeben. Mehr Infos in " + event.getGuild().getTextChannelById("470283820881543188").getAsMention());
                         break;
                     case 1:
-                        embed.setTitle("#TolkienTrotztCorona");
-                        embed.setDescription("Am Mittwoch den 25.03. und am Sonntag den 29.03. ab jeweils 18 Uhr, wird im Lyrikecke-Voicekanal aus Werken von Tolkien gelesen.");
+                        embed.setTitle("\uD83C\uDFA8 Der Zeichenwettbewerb");
+                        embed.setDescription("Einmal im Monat findet der Zeichenwettbewerb statt. Aus drei Themen wird eines gew\u00e4hlt, welches dann gezeichnet wird. Dabei soll kreativ gearbeitet werden und es gibt auch was zu gewinnen. " +
+                                "Alle Informationen in " + event.getGuild().getTextChannelById("557328633606307892").getAsMention());
                         break;
                     case 2:
-                        embed.setTitle("#TolkienTrotztCorona");
-                        embed.setDescription("Lesen vereint! Und das nicht nur w\u00e4hrend des Tolkien Reading Day, sondern auch zum Beispiel im Lyrikabend, der hier w\u00f6chentlich stattfindet. \n" +
-                                "Informationen hierzu: " + event.getGuild().getTextChannelById("470283820881543188").getAsMention());
+                        embed.setTitle("\uD83D\uDCDD Der Schreibwettbewerb");
+                        embed.setDescription("Alle zwei Monate findet der Schreibwettbewerb statt. Aus drei Themen wird eines gew\u00e4hlt, zu welchem dann ein Text geschrieben wird. Dabei soll kreativ gearbeitet werden und es gibt auch was zu gewinnen. " +
+                                "Alle Informationen in " + event.getGuild().getTextChannelById("596263709765271552").getAsMention());
                         break;
                     case 3:
-                        embed.setTitle("#TolkienTrotztCorona");
-                        embed.setDescription("Zwischen den Hauptevents des Tolkien Reading Days kann jeder Zuh\u00f6rer selbst Gedichte oder Briefe von J.R.R. Tolkien vorschlagen oder sogar vorlesen!");
+                        embed.setTitle("\u2753 Das Tolkien-Quick-Quiz");
+                        embed.setDescription("Jede Woche am Dienstag gibt es 10 Fragen zu Tolkiens Werken. Jeder darf teilnehmen (Au\u00dfer Experten und das Adminteam)\n" +
+                                "In " + event.getGuild().getTextChannelById("631157118942052362").getAsMention() + " findet es statt.");
                         break;
                     case 4:
-                        embed.setTitle("#TolkienTrotztCorona");
-                        embed.setDescription("Ein R\u00e4tsel in der Finsternis: Bilbo trifft das Gesch\u00f6pf Gollum und ein R\u00e4tselspiel um Leben und Tod entsteht.\n" +
-                                "Mittwoch 25.03. um 18 Uhr im Lyrikecke-Voicekanal.");
+                        embed.setTitle("\uD83D\uDCC5 W\u00f6chentliches Thema");
+                        embed.setDescription("(Fast) jede Woche kommt hier ein neues Thema, \u00fcber das diskutiert werden kann. Dabei muss man nicht die tiefste Einsicht in das Thema haben und kann auch einfach Fragen zu dem Thema stellen. " +
+                                event.getGuild().getTextChannelById("473261177397444620").getAsMention());
                         break;
                     case 5:
-                        embed.setTitle("#TolkienTrotztCorona");
-                        embed.setDescription("T\u00farin Turambar der tragische Held: Mehr \u00fcber ihn am Mittwoch den 25.03. ab 18 Uhr.");
+                        embed.setTitle("\u2754 Fragen zu Tolkiens Werken");
+                        embed.setDescription("Du willst wissen, wer Tom Bombadil war oder was die Schattenarmee gemacht hat?\n" +
+                                "Einfach in " + event.getGuild().getTextChannelById("453541314706014228").getAsMention() + " jede erdenkliche Frage zu Tolkien und/oder seinen Werken stellen!");
                         break;
                     case 6:
-                        embed.setTitle("#TolkienTrotztCorona");
-                        embed.setDescription("Die Abenteuer des Tom Bombadil noch nie selbst gelesen? Vorlesen lassen ist besser! \n" +
-                                "Sonntag 29.03. ab 18 Uhr.");
+                        embed.setTitle("\u23ef\ufe0f YouTuber-Kategorien");
+                        embed.setDescription("F\u00fcr alle hier vertretenen YouTuber gibt es einen Subbereich, wo ihr mit ihnen schreiben k\u00f6nnt und das Neuste erf\u00e4hrt.\n" +
+                                "Um beizutreten m\u00fcsst ihr nur einen einfachen Befehl eingeben:\n" +
+                                "Ardko: `/role add sub2Ardko`\n" +
+                                "Mythen aus Mittelerde: `/role add sub2MaM`\n" +
+                                "Tolkien erkl\u00e4rt: `/role add sub2TE`");
                         break;
 
                 }
--- src/main/java/util/STATIC.java
+++ src/main/java/util/STATIC.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class STATIC {
 
-    public static final String VERSION = "v2.9.10";
+    public static final String VERSION = "v2.9.11";
 
     public static final String PREFIX = "/";
 
